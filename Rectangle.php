7bb50b750b93b2c6253f14d52b4b225e9a7d1ea0
Lương Minh Đức <57982097+minhduc19669@users.noreply.github.com>
2020-04-15 09:00:00 +0700

    commit
    php

---
+++ Rectangle.php
@@ -0,0 +1,49 @@
+<?php
+
+include_once ('Shape.php');
+include_once ('Resizeable.php');
+
+class Rectangle extends Shape implements Resizeable
+{
+    public $width;
+    public $height;
+
+    public function __construct($name, $width, $height)
+    {
+        parent::__construct($name);
+        $this->width = $width;
+        $this->height = $height;
+    }
+
+    public function calculateArea(){
+        return $this->height * $this->width;
+    }
+
+    public function calculatePerimeter(){
+        return ($this->height + $this->width) * 2;
+   }
+   public function resize($random)
+   {
+       $this->width=$this->width+($this->width*$random/100);
+   }
+
+    /**
+     * @return mixed
+     */
+    public function getWidth()
+    {
+        return $this->width;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHeight()
+    {
+        return $this->height;
+    }
+
+    /**
+     * @return mixed
+     */
+}
